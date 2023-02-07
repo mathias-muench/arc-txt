1cbe74043e3919bdadebdd528f6064cf5d812675
Mathias Münch <mathias.muench@mathias-muench.de>
2023-02-07 20:07:48 +0100

    Works using iuml

--- arctxt.py
+++ arctxt.py
@@ -6,39 +6,30 @@ import glob
 from pathlib import Path
 
 iuml = """
-!global $levels = 0
-!procedure $close_boundary()
-!while $levels > 0
-}
-!$levels = $levels - 1
-!endwhile
-!endprocedure
-
-{% for sbb in sbbs %}
-{% set id = sbb.Type ~ "__" ~ sbb.Name %}
-!function ${{ id }}()
-{% if sbb.BType != "" %}
-%call_user_func("${{ sbb.BType }}__{{ sbb.BName }}")
-!$levels = $levels + 1
+{% for id, sbb in sbbs.items() %}
+!procedure ${{ id }}()
+{% if sbb.Enterprise != "" %}
+Enterprise_Boundary({{ sbb.Enterprise }}, "{{ sbbs[sbb.Enterprise].Label }}") {
 {% endif %}
-{% if sbb.Type in ["Enterprise_Boundary", "System_Boundary"] %}
-{{ sbb.Type }}({{ id }}, "{{ sbb.Label }}") {
-{% else %}
 {{ sbb.Type }}({{ id }}, "{{ sbb.Label }}", "{{ sbb.Description }}")
-$close_boundary()
+{% if sbb.Enterprise != "" %}
+}
 {% endif %}
-!return {{ id }}
-!endfunction
+!endprocedure
 
 {% endfor %}
 """
 
 with open("sbb.tsv", "r") as tsv_file:
-    sbbs = csv.DictReader(tsv_file, dialect="excel-tab")
-    e = jinja2.Environment(trim_blocks=True, lstrip_blocks=True)
-    t = e.from_string(iuml)
+    sbbs = dict()
+    for row in csv.DictReader(tsv_file, dialect="excel-tab"):
+        id = row["Type"] + "_" + row["Name"]
+        sbbs[id] = row
+
+    env = jinja2.Environment(trim_blocks=True, lstrip_blocks=True)
+    template = env.from_string(iuml)
     with open("sbb.iuml", "w") as iuml_file:
-        iuml_file.write(t.render(sbbs=list(sbbs)))
+        iuml_file.write(template.render(sbbs=sbbs))
 
 puml = """
 @startuml
@@ -46,7 +37,9 @@ puml = """
 !include ./sbb.iuml
 
 {% for i in rels %}
-Rel(${{ i.SType ~ '__' ~ i.SName  }}(), ${{ i.DType ~ '__' ~ i.DName }}(), "{{ i.Label }}", "{{ i[view] }}")
+${{ i.SType ~ '_' ~ i.SName  }}()
+${{ i.DType ~ '_' ~ i.DName }}()
+Rel({{ i.SType ~ '_' ~ i.SName  }}, {{ i.DType ~ '_' ~ i.DName }}, "{{ i.Label }}", "{{ i[view] }}")
 {% endfor %}
 
 'LAYOUT_LANDSCAPE()
@@ -58,10 +51,10 @@ views = map(lambda f: Path(f).stem, glob.glob("*-*.tsv"))
 for view in views:
     with open(f"{view}.tsv", "r") as tsv_file:
         rels = csv.DictReader(tsv_file, dialect="excel-tab")
-        e = jinja2.Environment(trim_blocks=True, lstrip_blocks=True)
-        t = e.from_string(puml)
+        env = jinja2.Environment(trim_blocks=True, lstrip_blocks=True)
+        template = env.from_string(puml)
         with open(f"{view}.puml", "w") as puml_file:
-            puml_file.write(t.render(rels=list(rels), view="Authentication"))
+            puml_file.write(template.render(rels=list(rels), view="Authentication"))
 
     for format in []:
         subprocess.run(
