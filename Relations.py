c3d0aef92a4e87bfeb40c5d92591e61bb176a8b1
Mathias Münch <mathias.muench@mathias-muench.de>
2023-02-13 09:26:06 +0100

    Relations unittest

--- Relations.py
+++ Relations.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class Relations:
     def __init__(self, relations: list):
         self.rels = list()
@@ -17,3 +20,48 @@ class Relations:
                 if rel[i] not in used:
                     used.append(rel[i])
         return used
+
+
+class TestRelations(unittest.TestCase):
+    def test_used_sbbs(self):
+        relations = [
+            {
+                "SType": "SBB",
+                "SName": "SBB1",
+                "DType": "SBB",
+                "DName": "SBB2",
+                "Label": "Label1",
+            },
+            {
+                "SType": "SBB",
+                "SName": "SBB1",
+                "DType": "SBB",
+                "DName": "SBB3",
+                "Label": "Label2",
+            },
+            {
+                "SType": "SBB",
+                "SName": "SBB2",
+                "DType": "SBB",
+                "DName": "SBB3",
+                "Label": "Label3",
+            },
+            {
+                "SType": "SBB",
+                "SName": "SBB4",
+                "DType": "SBB",
+                "DName": "SBB5",
+                "Label": "Label4",
+            },
+        ]
+        rels = Relations(relations)
+        self.assertEqual(
+            rels.used_sbbs(),
+            [
+                ("SBB", "SBB1"),
+                ("SBB", "SBB2"),
+                ("SBB", "SBB3"),
+                ("SBB", "SBB4"),
+                ("SBB", "SBB5"),
+            ],
+        )
